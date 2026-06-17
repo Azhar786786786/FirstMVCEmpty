726c5415d84e21fdb5cc3ffb1156dbf4cb14c148
Azhar786786786 <talkazhar786@hotmail.com>
2026-06-17 16:57:17 +0530

    Added Controllers and changes configuration

---
+++ FirstMVCEmpty/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Azhar hussain is my name.";
+        }
+        public string Name()
+        {
+            return "This is my name";
+        }
+    }
+}
--- FirstMVCEmpty/Program.cs
+++ FirstMVCEmpty/Program.cs
@@ -1,24 +1,32 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//All services register here
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
-app.Use((context, next) =>
-{
-    context.Response.WriteAsync("This is Use1 middleware and now call the next middleware.\n");
-    return next();
-});
+//app.Use((context, next) =>
+//{
+//    context.Response.WriteAsync("This is Use1 middleware and now call the next middleware.\n");
+//    return next();
+//});
 
-app.Use((context, next) =>
-{
-    context.Response.WriteAsync("This is Use2 middleware and now call the next middleware.\n");
-    return next();
-});
+//app.Use((context, next) =>
+//{
+//    context.Response.WriteAsync("This is Use2 middleware and now call the next middleware.\n");
+//    return next();
+//});
 
-app.Run((context) => context.Response.WriteAsync("it is terminates the pipline means run middleware"));
+//app.Run((context) => context.Response.WriteAsync("it is terminates the pipline means run middleware"));
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/first", () => "Hello World first!");
-app.MapGet("/second", () => "Hello World second!");
-app.MapGet("/third", () => "Hello World third!");
+//app.MapGet("/first", () => "Hello World first!");
+//app.MapGet("/second", () => "Hello World second!");
+//app.MapGet("/third", () => "Hello World third!");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
