2d7d4f92fd00198b2792edb6448691c4ef5052a6
Azhar786786786 <talkazhar786@hotmail.com>
2026-06-17 16:38:46 +0530

    Add project files.

---
+++ FirstMVCEmpty/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Use((context, next) =>
+{
+    context.Response.WriteAsync("This is Use1 middleware and now call the next middleware.\n");
+    return next();
+});
+
+app.Use((context, next) =>
+{
+    context.Response.WriteAsync("This is Use2 middleware and now call the next middleware.\n");
+    return next();
+});
+
+app.Run((context) => context.Response.WriteAsync("it is terminates the pipline means run middleware"));
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/first", () => "Hello World first!");
+app.MapGet("/second", () => "Hello World second!");
+app.MapGet("/third", () => "Hello World third!");
+
+app.Run();
